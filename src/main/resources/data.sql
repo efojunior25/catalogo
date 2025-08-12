4e862363512355bc3ebb9a914db1826c460a449b
Edson Júnior <efojunior25@gmail.com>
2025-08-12 12:36:50 -0300

    Initial Commit - setup project & database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- Insert Products H2 & MySQL
+INSERT INTO products (name, price, stock, active) VALUES
+('Café Torrado 500g', 18.90, 5, true),
+('Filtro de Papel nº103', 7.50, 10, true),
+('Garrafa Térmica 1L', 79.90, 2, true),
+('Açúcar Mascavo 1Kg', 16.00, 0, true),
+('Caneca Inox 300ml', 29.00, 8, true)
