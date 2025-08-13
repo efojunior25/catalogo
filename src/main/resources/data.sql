af8c4c4969eb974e923414438eca60807bc249a1
Edson Júnior <efojunior25@gmail.com>
2025-08-12 23:34:58 -0300

    add exceptions, stockerrorDTO, add function Order Service, fix import data

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
--- Insert Products H2 & MySQL
-INSERT INTO products (name, price, stock, active, version) VALUES
-('Café Torrado 500g', 18.90, 5, true, 0),
-('Filtro de Papel nº103', 7.50, 10, true,0),
-('Garrafa Térmica 1L', 79.90, 2, true, 0),
-('Açúcar Mascavo 1Kg', 16.00, 0, true, 0),
-('Caneca Inox 300ml', 29.00, 8, true, 0)
